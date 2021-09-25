c06ac0d31e2b74fe5a6edf6533cc77e4ccc89f05
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2021-09-25 18:45:37 +0530

    [nadeem]added:empWagecal

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,11 +5,15 @@ echo "Welcome to the EmpWage problem."
 randomCheck=$((RANDOM%2)) # 0/1
 #constant varible
 IS_PRESENT=1
-
+emprate=20
+empHrs=8
 #selection
 if [ $IS_PRESENT -eq $randomCheck ]
 then
+      salary=$((empHrs*emprate))
       echo "emp is present"
 else
+      salary=0
       echo "emp is absent"
 fi
+
