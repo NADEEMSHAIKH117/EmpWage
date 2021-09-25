ae592f175ab99e51e337dc26bd366f2df744bf55
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2021-09-25 18:26:54 +0530

    [nadeem]added:EmpAttendence feature

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,15 @@
 #!/bin/bash -x
 
 echo "Welcome to the EmpWage problem."
+#variable
+randomCheck=$((RANDOM%2)) # 0/1
+#constant varible
+IS_PRESENT=1
+
+#selection
+if [ $IS_PRESENT -eq $randomCheck ]
+then
+      echo "emp is present"
+else
+      echo "emp is absent"
+fi
